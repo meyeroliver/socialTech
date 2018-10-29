1a93105a04732a28b86666d9f7210410b14ff38f
Linda MUCASSI <lmucassi@e4r7p6.wethinkcode.co.za>
2018-10-29 17:38:20 +0200

    Do: del[ not done ]

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,16 +2,42 @@ const express = require('express')
 const router = express.Router()
 
 // product model
-const product = require('../../models/products')
+const Product = require('../../models/products')
 
+// GET
 // @route   GET api/products
 // @desc    GET all products
 // @access  public
-router.get('/', (req, res) => {
-    product
-    .find()
+/*router.get('/', (req, res) => {
+    Product.find()
     .sort({ date: -1 })
     .then(products => res.json(products))
+}); // because we are already inside the required path */
+
+// POST
+// @route   POST api/products
+// @desc    Create a product
+// @access  public
+router.post('/', (req, res) => {
+    //create in memory
+    const newProduct = new Product({
+        name: req.body.name
+    });
+
+    // save
+    newProduct.save().then(product => res.json(product));
+}); // because we are already inside the required path
+
+
+// DEL
+// @route   DEL api/products
+// @desc    Delete a product
+// @access  public
+router.delete('/:id', (req, res) => {
+    
+    product.findById(req.params.id).then(product => remove()
+        .then(() => res.json({success: true}))
+
 }); // because we are already inside the required path
 
 module.exports = router
